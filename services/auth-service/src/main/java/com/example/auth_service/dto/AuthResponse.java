f47dbae521d2cd289924d5a3d139f50d3a6b23fb
SaifAlqady51 <saifalqady51@gmail.com>
2025-06-19 20:30:25 +0300

    feat: Creating dto for auth-service

---
+++ services/auth-service/src/main/java/com/example/auth_service/dto/AuthResponse.java
@@ -0,0 +1,29 @@
+package com.example.auth_service.dto;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+    private String token;
+    private String refreshToken;
+    private UUID userId;
+    private String username;
+    private String email;
+    private String avatarUrl;
+    private String status;
+    private Instant expiresAt;
+
+    public String getEmail() {
+        return email;
+    }
+
+}
