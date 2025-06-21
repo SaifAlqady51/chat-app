a26f9733870dc8c7ef73e301dd9c23136547bcef
SaifAlqady51 <saifalqady51@gmail.com>
2025-06-21 23:08:29 +0300

    feat: Edited test folder

--- services/auth-service/src/test/java/com/example/auth_service/AuthServiceApplicationTests.java
+++ services/auth-service/src/test/java/com/example/auth_service/AuthServiceApplicationTests.java
@@ -1,8 +1,10 @@
-package auth_service;
+package com.example.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class AuthServiceApplicationTests {
 
