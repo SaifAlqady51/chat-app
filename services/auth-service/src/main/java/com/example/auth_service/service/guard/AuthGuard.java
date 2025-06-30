700c8fcbb2077c241220854892947ac4c9e0251e
SaifAlqady51 <saifalqady51@gmail.com>
2025-06-30 19:38:02 +0300

    refactor: Edited AuthGuards

--- services/auth-service/src/main/java/com/example/auth_service/service/guard/AuthGuard.java
+++ services/auth-service/src/main/java/com/example/auth_service/service/guard/AuthGuard.java
@@ -1,10 +1,13 @@
 package com.example.auth_service.service.guard;
 
+import com.example.auth_service.dto.LoginRequest;
 import com.example.auth_service.dto.RegisterRequest;
+import com.example.auth_service.entity.User;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 public interface AuthGuard {
     void checkRegistrationEligibility(RegisterRequest request) throws ResponseStatusException;
-   // void validateLoginRequest(LoginRequest request) throws ResponseStatusException;
-    //void validateRefreshTokenRequest(RefreshTokenRequest request) throws ResponseStatusException;
+    Optional<User> checkLoginAndGetUser(LoginRequest request) throws ResponseStatusException;
 }
