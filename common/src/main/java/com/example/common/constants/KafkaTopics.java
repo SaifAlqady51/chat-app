8deba733aa97a42c80a25fb0a6ee716111aef17c
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-16 16:56:23 +0300

    feat: Created KafkaTopics file

---
+++ common/src/main/java/com/example/common/constants/KafkaTopics.java
@@ -0,0 +1,8 @@
+package com.example.common.constants;
+public class KafkaTopics {
+
+    public static final String USER_EXISTENCE_CHECK_BY_ID = "user-existence-check-by-id";
+    private KafkaTopics() {
+        // Private constructor to prevent instantiation
+    }
+}
