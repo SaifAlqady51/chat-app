6da9ec98c9fdaec545a9a4ded7b62774c3ec24ea
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-19 23:29:18 +0300

    feat: Added topics to KafkaTopics

--- common/src/main/java/com/example/common/constants/KafkaTopics.java
+++ common/src/main/java/com/example/common/constants/KafkaTopics.java
@@ -2,6 +2,8 @@ package com.example.common.constants;
 public class KafkaTopics {
 
     public static final String USER_EXISTENCE_CHECK_BY_ID = "user-existence-check-by-id";
+    public static final String USER_EXISTENCE_REQUEST = "user-existence-request";
+    public static final String USER_EXISTENCE_RESPONSE = "user-existence-response";
     private KafkaTopics() {
         // Private constructor to prevent instantiation
     }
