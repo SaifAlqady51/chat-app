8e26af7da04389e123160f49ba7ebe57c758917a
SaifAlqady51 <saifalqady51@gmail.com>
2025-07-23 18:55:07 +0300

    refactor: Edited KafkaTopics names

--- common/src/main/java/com/example/common/constants/KafkaTopics.java
+++ common/src/main/java/com/example/common/constants/KafkaTopics.java
@@ -1,10 +1,9 @@
 package com.example.common.constants;
-public class KafkaTopics {
-
-    public static final String USER_EXISTENCE_CHECK_BY_ID = "user-existence-check-by-id";
+public final class KafkaTopics {
     public static final String USER_EXISTENCE_REQUEST = "user-existence-request";
     public static final String USER_EXISTENCE_RESPONSE = "user-existence-response";
+
     private KafkaTopics() {
-        // Private constructor to prevent instantiation
+        throw new AssertionError("Cannot instantiate constants class");
     }
 }
